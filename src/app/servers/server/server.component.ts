f775429fd3636ce61cecbccc98bd008d425242a8
Resao <mail@christophermiles.co.uk>
2017-06-15 20:46:39 +0100

    Only allow server edits if param is passed

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,17 +1,17 @@
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import {Component, OnInit} from '@angular/core';
 
 import {ServersService} from '../servers.service';
 
 @Component({selector: 'app-server', templateUrl: './server.component.html', styleUrls: ['./server.component.css']})
 export class ServerComponent implements OnInit {
-  server : {
+  server: {
     id: number,
     name: string,
     status: string
   };
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) {}
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
@@ -21,6 +21,10 @@ export class ServerComponent implements OnInit {
         this.server = this.serversService.getServer(+params['id']);
       }
     )
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 
 }
