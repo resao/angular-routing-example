fb691f1a11d15bc74be658f436d8ca836d06a56d
Resao <mail@christophermiles.co.uk>
2017-06-16 14:17:46 +0100

    Add login and logout buttons to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
+import { AuthService } from './../auth.service';
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({selector: 'app-home', templateUrl: './home.component.html', styleUrls: ['./home.component.css']})
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {}
 
@@ -20,5 +21,13 @@ export class HomeComponent implements OnInit {
         },
         fragment: 'loading'
       });
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
